cd98bacda938c315b4fae368397b96115ec1a3b7
YJK-Tech <yjktechnologiesdev@gmail.com>
2026-02-24 13:49:12 +0530

    Bug Fixing

--- src/Apiconfig.js
+++ src/Apiconfig.js
@@ -1,5 +1,6 @@
 const config = {
-    apiBaseUrl: 'http://95.216.47.253:5499'
+    // apiBaseUrl: 'http://95.216.47.253:5499'
+    apiBaseUrl: 'https://erpdev.yjktechnologies.com:5510'
   };
   
   module.exports = config;
